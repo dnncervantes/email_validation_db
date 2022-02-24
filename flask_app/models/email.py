cda99b5c54940a09a047da5332695c6b54c88b12
Danniel C <dnncervantes.yahoo.com>
2022-02-23 18:38:34 -0800

    first commit

---
+++ flask_app/models/email.py
@@ -0,0 +1,45 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+
+class Email:
+    def __init__(self, data):
+        self.id = data['id']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO emails (email) VALUES (%(email)s);"
+        return connectToMySQL('email_validation').query_db(query,data)
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM emails;"
+        results = connectToMySQL('email_validation').query_db(query)
+        emails = []
+        for email in results:
+            emails.append(cls(email))
+        return emails
+
+    @classmethod
+    def exterminate(cls,data):
+        query = "DELETE FROM emails WHERE id = %(id)s;"
+        return connectToMySQL('email_validation').query_db(query,data)
+
+    @staticmethod
+    def is_valid(email):
+        is_valid = True
+        query = "SELECT * FROM emails WHERE email = %(email)s;"
+        results = connectToMySQL('email_validation').query_db(query,email)
+        if len(results) >= 1:
+            flash("Existing Email")
+            is_valid = False
+        if not EMAIL_REGEX.match(email['email']):
+            flash("Invalid Email")
+            is_valid = False
+        return is_valid
